ec2785bf5a2a1517cef689a330d5ad32703c05c1
kssk3 <kss8014@gmail.com>
2025-04-24 17:27:20 +0900

    docs : 정리

---
+++ src/main/java/lambda/ex3/FilterExampleEx2.java
@@ -0,0 +1,27 @@
+package lambda.ex3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class FilterExampleEx2 {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(-3, -2, -1, 1, 2, 3, 5);
+        System.out.println("원본 리스트 : " + numbers);
+
+        System.out.println("음수만 : " + filter(numbers, value -> value < 0));
+        System.out.println("짝수만 : " + filter(numbers, value -> value % 2 == 0));
+    }
+
+    static List<Integer> filter(List<Integer> list, Predicate<Integer> predicate) {
+        List<Integer> result = new ArrayList<>();
+        for (Integer i : list) {
+            if(predicate.test(i)){result.add(i);}
+        }
+        return result;
+    }
+}
+
+
---
+++ src/main/java/lambda/lambda4/BiPredicateMain.java
@@ -0,0 +1,21 @@
+package lambda.lambda4;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+
+public class BiPredicateMain {
+
+    public static void main(String[] args) {
+        BiPredicate<Integer, Integer> biPredicate = (s, n) -> s > n;
+        System.out.println("biPredicate.test(1, 2) = " + biPredicate.test(1, 2));
+
+        BiConsumer<Integer, String> biConsumer = (s , n) -> {
+            for(int i = 0; i < s; i++){
+                System.out.print(n + " ");
+            }
+            System.out.println();
+        };
+        biConsumer.accept(8, "*");
+
+    }
+}
