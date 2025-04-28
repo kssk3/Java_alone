33ee8dde946ddcd0ca4bb3af6f766a0460be0801
kssk3 <kss8014@gmail.com>
2025-04-28 23:20:31 +0900

    docs : 메서드 참조

--- src/main/java/methodref/start/MethodRefStartV2.java
+++ src/main/java/methodref/start/MethodRefStartV2.java
@@ -1,17 +1,21 @@
-package methodref;
+package methodref.start;
 
 import java.util.function.BinaryOperator;
 
-public class MethodRefStartV1 {
+public class MethodRefStartV2 {
 
     public static void main(String[] args) {
-        BinaryOperator<Integer> add1 = (x,y) -> x + y;
-        BinaryOperator<Integer> add2 = (x,y) -> x + y;
+        BinaryOperator<Integer> add1 = MethodRefStartV2::add;
+        BinaryOperator<Integer> add2 = MethodRefStartV2::add;
 
         Integer result1 = add1.apply(1, 2);
         System.out.println("result1 = " + result1);
 
         Integer result2 = add2.apply(1, 2);
         System.out.println("result2 = " + result2);
+    }
+
+    static int add(int a, int b) {
+        return a + b;
     }
 }
--- src/main/java/methodref/start/MethodRefStartV3.java
+++ src/main/java/methodref/start/MethodRefStartV3.java
@@ -1,12 +1,12 @@
-package methodref;
+package methodref.start;
 
 import java.util.function.BinaryOperator;
 
-public class MethodRefStartV2 {
+public class MethodRefStartV3 {
 
     public static void main(String[] args) {
-        BinaryOperator<Integer> add1 = MethodRefStartV2::add;
-        BinaryOperator<Integer> add2 = MethodRefStartV2::add;
+        BinaryOperator<Integer> add1 = (x,y) -> add(x,y);
+        BinaryOperator<Integer> add2 = (x,y) -> add(x,y);
 
         Integer result1 = add1.apply(1, 2);
         System.out.println("result1 = " + result1);
