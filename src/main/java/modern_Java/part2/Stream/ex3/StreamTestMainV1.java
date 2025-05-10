35280138bdd18c96170701e3213fe8bcd55425bb
kssk3 <kss8014@gmail.com>
2025-05-10 17:05:20 +0900

    docs : Modern Java Part2 4.3.1

---
+++ src/main/java/modern_Java/part2/Stream/ex3/StreamTestMainV1.java
@@ -0,0 +1,13 @@
+package modern_Java.part2.Stream.ex3;
+
+import java.util.List;
+
+public class StreamTestMainV1 {
+    public static void main(String[] args) {
+
+        List<String> title = List.of("java8", "In", "Action");
+
+        title.stream()
+                .forEach(System.out::println);
+    }
+}
